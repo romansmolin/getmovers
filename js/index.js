621fa2dd8228a564feec1a9a8e4243281a855ced
Roman Smolin <romasmolin29@gmail.com>
2023-11-20 16:12:15 +0200

    Minor fixes

--- js/index.js
+++ js/index.js
@@ -1,79 +1,79 @@
 function navigateToURL(element) {
-    const url = element.getAttribute("data-href");
-    window.location.href = url;
+  const url = element.getAttribute("data-href");
+  window.location.href = url;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    function updateProgressBar(progress) {
-        const progressBarItems = document.querySelectorAll('.progress__bar__item');
-        for (let i = 0; i < progressBarItems.length; i++) {
-            if (i < progress) {
-                progressBarItems[i].style.backgroundColor = '#dc3545';
-            } else {
-                progressBarItems[i].style.backgroundColor = '#D9D9D9';
-            }
-        }
+document.addEventListener("DOMContentLoaded", function () {
+  function updateProgressBar(progress) {
+    const progressBarItems = document.querySelectorAll(".progress__bar__item");
+    for (let i = 0; i < progressBarItems.length; i++) {
+      if (i < progress) {
+        progressBarItems[i].style.backgroundColor = "#dc3545";
+      } else {
+        progressBarItems[i].style.backgroundColor = "#D9D9D9";
+      }
     }
+  }
 
-    function getPageIdentifier() {
-        const urlSegments = window.location.pathname.split('/');
-        return urlSegments[urlSegments.length - 1];
-    }
+  function getPageIdentifier() {
+    const urlSegments = window.location.pathname.split("/");
+    return urlSegments[urlSegments.length - 1];
+  }
 
-    const currentPageIdentifier = getPageIdentifier();
+  const currentPageIdentifier = getPageIdentifier();
 
-    // Determine progress based on the current page
-    let progress;
-    switch (currentPageIdentifier) {
-        case 'index.html':
-        case 'MovingFrom.html':
-        case 'CurrentHouseSize.html':
-            progress = 1;
-            break;
-        case 'CompareTo.html':
-        case 'WhenAreYouMoving.html':
-        case 'Destination.html':
-            progress = 2;
-            break;
-        case 'Email.html':
-        case 'EnterName.html':
-        case 'PhoneNumber.html':
-            progress = 3;
-            break;
-        case 'FinalPage.html':
-            progress = 4;
-            break;
-        default:
-            progress = 0;
-    }
+  // Determine progress based on the current page
+  let progress;
+  switch (currentPageIdentifier) {
+    case "index.html":
+    case "MovingFrom.html":
+    case "CurrentHouseSize.html":
+      progress = 1;
+      break;
+    case "CompareTo.html":
+    case "WhenAreYouMoving.html":
+    case "Destination.html":
+      progress = 2;
+      break;
+    case "Email.html":
+    case "EnterName.html":
+    case "PhoneNumber.html":
+      progress = 3;
+      break;
+    case "FinalPage.html":
+      progress = 4;
+      break;
+    default:
+      progress = 0;
+  }
 
-    updateProgressBar(progress);
+  updateProgressBar(progress);
 });
 
-
 window.onload = () => {
-    const transitionElements = document.querySelector('.transition')
-    const anchorsElements = document.querySelectorAll('a');
+  const transitionElements = document.querySelector(".transition");
+  const anchorsElements = document.querySelectorAll("a");
 
-    setTimeout(() => {
-        transitionElements.classList.remove('is-active')
-    }, 500)
+  setTimeout(() => {
+    transitionElements.classList.remove("is-active");
+  }, 500);
 
-    for (let i = 0; i < anchorsElements.length; i++) {
-        const anchor = anchorsElements[i]
+  for (let i = 0; i < anchorsElements.length; i++) {
+    const anchor = anchorsElements[i];
 
-        anchor.addEventListener('click', e => {
-            e.preventDefault();
-            let target = e.currentTarget.href
+    anchor.addEventListener("click", (e) => {
+      if (anchor.id === "phone-number-link") {
+        return
+      }
 
-            transitionElements.classList.add('is-active')
+      e.preventDefault();
+      let target = e.currentTarget.href;
 
-            setTimeout(() => {
-                window.location.href = target;
-            }, 500)
-        })
-    }
-    
-}
+      transitionElements.classList.add("is-active");
 
-
+      setTimeout(() => {
+        window.location.href = target;
+      }, 500);
+    });
+  }
+};
