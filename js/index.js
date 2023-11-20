23953a0078c70f1ff1c3690d3e80be460ec67574
Roman Smolin <romasmolin29@gmail.com>
2023-11-20 15:14:02 +0200

    Progress bar

--- js/index.js
+++ js/index.js
@@ -3,6 +3,54 @@ function navigateToURL(element) {
     window.location.href = url;
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    function updateProgressBar(progress) {
+        const progressBarItems = document.querySelectorAll('.progress__bar__item');
+        for (let i = 0; i < progressBarItems.length; i++) {
+            if (i < progress) {
+                progressBarItems[i].style.backgroundColor = '#dc3545';
+            } else {
+                progressBarItems[i].style.backgroundColor = '#D9D9D9';
+            }
+        }
+    }
+
+    function getPageIdentifier() {
+        const urlSegments = window.location.pathname.split('/');
+        return urlSegments[urlSegments.length - 1];
+    }
+
+    const currentPageIdentifier = getPageIdentifier();
+
+    // Determine progress based on the current page
+    let progress;
+    switch (currentPageIdentifier) {
+        case 'index.html':
+        case 'MovingFrom.html':
+        case 'CurrentHouseSize.html':
+            progress = 1;
+            break;
+        case 'CompareTo.html':
+        case 'WhenAreYouMoving.html':
+        case 'Destination.html':
+            progress = 2;
+            break;
+        case 'Email.html':
+        case 'EnterName.html':
+        case 'PhoneNumber.html':
+            progress = 3;
+            break;
+        case 'FinalPage.html':
+            progress = 4;
+            break;
+        default:
+            progress = 0;
+    }
+
+    updateProgressBar(progress);
+});
+
+
 window.onload = () => {
     const transitionElements = document.querySelector('.transition')
     const anchorsElements = document.querySelectorAll('a');
