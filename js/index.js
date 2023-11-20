da17b932099369dda93ccbea2323be5b6b450a9e
Roman Smolin <romasmolin29@gmail.com>
2023-11-20 14:05:13 +0200

    Transition effect

--- js/index.js
+++ js/index.js
@@ -2,3 +2,30 @@ function navigateToURL(element) {
     const url = element.getAttribute("data-href");
     window.location.href = url;
 }
+
+window.onload = () => {
+    const transitionElements = document.querySelector('.transition')
+    const anchorsElements = document.querySelectorAll('a');
+
+    setTimeout(() => {
+        transitionElements.classList.remove('is-active')
+    }, 500)
+
+    for (let i = 0; i < anchorsElements.length; i++) {
+        const anchor = anchorsElements[i]
+
+        anchor.addEventListener('click', e => {
+            e.preventDefault();
+            let target = e.currentTarget.href
+
+            transitionElements.classList.add('is-active')
+
+            setTimeout(() => {
+                window.location.href = target;
+            }, 500)
+        })
+    }
+    
+}
+
+
